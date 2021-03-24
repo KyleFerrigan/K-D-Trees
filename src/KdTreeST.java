caf7b15c099f076cd34462ff536548d194637fa3
kbferrigan <kbferrigan@outlook.com>
2021-03-24 14:30:06 -0500

    initialize

--- src/KdTreeST.java
+++ src/KdTreeST.java
@@ -18,4 +18,33 @@ public class KdTreeST<Value> {
 
     }
 
+    // value associated with point p
+    public Value get(Point2D p){
+        return null;//todo change
+    }
+
+    // does the symbol table contain point p?
+    public boolean contains(Point2D p){
+        return false; //todo change
+    }
+
+    // all points in the symbol table
+    public Iterable<Point2D> points(){
+        return null; //todo change
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect){
+        return null; //todo change
+    }
+
+    // a nearest neighbor of point p; null if the symbol table is empty
+    public Point2D nearest(Point2D p){
+        return null;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args){
+
+    }
 }
