9a5feb456778dd4243a419b91266b42d6f96a4e4
kbferrigan <kbferrigan@outlook.com>
2021-03-24 14:29:04 -0500

    initialize

--- src/KdTreeST.java
+++ src/KdTreeST.java
@@ -1,2 +1,21 @@
-public class KdTreeST {
+public class KdTreeST<Value> {
+    KdTreeST() {
+
+    }
+
+    // is the symbol table empty?
+    public boolean isEmpty() {
+        return false;//todo change
+    }
+
+    // number of points public
+    int size() {
+        return 0; //todo change
+    }
+
+    // associate the value val with point p
+    public void put(Point2D p, Value val){
+
+    }
+
 }
