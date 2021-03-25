d56dda80b1bc252eeda724f6feebb9d4cd6f8f2f
pagalati <78822760+pagalati@users.noreply.github.com>
2021-03-24 19:54:21 -0500

    Create KdTreeST.java

--- src/KdTreeST.java
+++ src/KdTreeST.java
@@ -8,36 +8,46 @@ public class KdTreeST<Value> {
         private RectHV rect; // the axis-aligned rectangle corresponding to this node
         private Node lb; // the left/bottom subtree
         private Node rt; // the right/top subtree
+        private boolean xCord; // the x/y coordinate status; x = true, y = false
+        
+        public Node(){
+            
+        }
+        public Node(Point2D p, Value val, RectHV rect, Node lb, Node rt, boolean xCord){
+            this.p = p;
+            this.val = val;
+            this.rect = rect;
+            this.lb = lb;
+            this.rt = rt;
+            this.xCord = xCord;
+        }
     }
 
     Node topNode;
     int nodeAmnt;
+    int depth = 0;
     double xmin;
     double ymin;
     double xmax;
     double ymax;
 
-
     // construct an empty set of points
     public KdTreeST(){
-        topNode = null;
-        nodeAmnt = 0;
+        Node topNode = new Node();
+        size++;
     }
 
     // is the symbol table empty?
     public boolean isEmpty() {
-        if (topNode == null) return true;
+        if (topNode.val == null) return true;
         else return false;
     }
 
     // number of points
-    public int size() { return nodeAmnt; }
-
-
-
+    public int size() { return size; }
 
     // associate the value val with point p
-    public void put(Point2D p, Value val){
+    public void put(Point2D p, Value val){ 
         if (p == null || val == null) throw new IllegalArgumentException("NULL");
         xmin = 0.0;
         ymin = 0.0;
@@ -72,28 +82,41 @@ public class KdTreeST<Value> {
         return n;
     }
     // value associated with point p
-    public Value get(Point2D p){
-
-    }
+    public Value get(Point2D p){  }
 
     // does the symbol table contain point p?
-    public boolean contains(Point2D p){
-
-    }
+    public boolean contains(Point2D p){  }
 
     // all points in the symbol table
-    public Iterable<Point2D> points(){
-
-    }
+    public Iterable<Point2D> points(){  }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect){
-
+        if (rect == null) throw new IllegalArgumentException("No Rectangle Found!");
+        Iterable<Point2D> inRect = rbBST.keys(); //grab all keys from rbBST
+        Iterator i = inRect.iterator(); //create an iterator on top of it
+        while (i.hasNext()){ // keep going until end of iterator
+            Point2D next = (Point2D) i.next();
+            if (!rect.contains(next)){ //if rectangle doesn't contain point
+                i.remove(); //remove said point
+            }
+        }
+        return inRect; //return iterator that only contains points within rectangle
     }
 
     // a nearest neighbor of point p; null if the symbol table is empty
     public Point2D nearest(Point2D p){
-
+        if (isEmpty() || p == null) throw new IllegalArgumentException("The Symbol Table is Empty!");
+        Point2D minP = null;
+        double dist, minDist = Double.MAX_VALUE;
+        for (Point2D neighbor: points()){
+            dist = p.distanceSquaredTo(neighbor);
+            if (dist < minDist){
+                minDist = dist;
+                minP = neighbor;
+            }
+        }
+        return minP;
     }
 
     // unit testing (required)
