8cf49279a832a425dcebc9d9d2dd52c639efe7ca
kbferrigan <kbferrigan@outlook.com>
2021-03-24 20:05:41 -0500

    Modified KdTreeST.java

--- src/KdTreeST.java
+++ src/KdTreeST.java
@@ -94,38 +94,23 @@ public class KdTreeST<Value> {
     }
 
     // does the symbol table contain point p?
-    public boolean contains(Point2D p){  }
+    public boolean contains(Point2D p){
+        
+    }
 
     // all points in the symbol table
-    public Iterable<Point2D> points(){  }
+    public Iterable<Point2D> points(){
+
+    }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect){
-        if (rect == null) throw new IllegalArgumentException("No Rectangle Found!");
-        Iterable<Point2D> inRect = rbBST.keys(); //grab all keys from rbBST
-        Iterator i = inRect.iterator(); //create an iterator on top of it
-        while (i.hasNext()){ // keep going until end of iterator
-            Point2D next = (Point2D) i.next();
-            if (!rect.contains(next)){ //if rectangle doesn't contain point
-                i.remove(); //remove said point
-            }
-        }
-        return inRect; //return iterator that only contains points within rectangle
+
     }
 
     // a nearest neighbor of point p; null if the symbol table is empty
     public Point2D nearest(Point2D p){
-        if (isEmpty() || p == null) throw new IllegalArgumentException("The Symbol Table is Empty!");
-        Point2D minP = null;
-        double dist, minDist = Double.MAX_VALUE;
-        for (Point2D neighbor: points()){
-            dist = p.distanceSquaredTo(neighbor);
-            if (dist < minDist){
-                minDist = dist;
-                minP = neighbor;
-            }
-        }
-        return minP;
+
     }
 
     // unit testing (required)
